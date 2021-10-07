438dc9ee88180c2a1ac25b5de8324b2171826cdf
Jacob Stannix <jakestannix at gmail.com>
2021-10-06 18:04:02 -0600

    changed arg flag

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,54 @@
 use structopt::StructOpt;
 use std::path::PathBuf;
-use std::io::{Error, ErrorKind};
+#[allow(unused_imports)]
+use raster::{self, Color, Image};
 
-/// holds all CLI arguments
+type RastrResult<T> = Result<T, RastrError>;
+
+#[derive(Debug)]
+pub struct RastrError(String); 	
+
 #[derive(StructOpt, Debug)]
+#[structopt(about = "get image info from the command line")]
 pub struct Cli {
+    /// Generate Histogram
+    #[structopt(short = "-g" , long)]
+    pub histogram: bool,
     /// The file to look for
     #[structopt(parse(from_os_str))]
     pub image: std::path::PathBuf,
 }
 
+
+/// runs logic on what functions to call.
+pub fn run_args(args: Cli) -> RastrResult<()> {
+    let image_path = get_image_path(args.image)?;
+    if args.histogram {
+	generate_histogram(image_path)?;
+    }
+    Ok(())
+}
+
+fn generate_histogram(_path: String) -> RastrResult<()> {
+    todo!{};
+    // Ok(())
+}
 /// Gets the absolute file path for the given input
-pub fn get_image_path(path: PathBuf) -> std::io::Result<String> {
-    let parent = match path.as_path().parent() {
-	Some(a) => a,
-	None => {
-	    return Err(Error::new(ErrorKind::NotFound, "directory not found"));
-	},
+pub fn get_image_path(path: PathBuf) -> RastrResult<String> {
+    let parent = path.as_path()
+	.canonicalize()
+	.map_err(|err|
+		 RastrError(format!("Error reading `{}`: {}",
+				    match path.to_str() {
+					Some(p) => p,
+					None => "hello",
+				    },
+				    err)))?;
+
+    let parent = match parent.to_str() {
+	Some(p) => { p },
+	None => { panic!("unknown error") },
     };
 
-    let parent = std::fs::canonicalize(parent)?;
-
-    let file_name = match path.file_name() {
-	Some(a) => a,
-	None => {
-	    return Err(Error::new(ErrorKind::NotFound, "no file found"));
-	}
-    };
-
-    let file_path = format!("{}/{}", parent.to_str()
-			    .ok_or(Error::new(
-				ErrorKind::Other, "couldn't get string from parent"))?,
-			    file_name.to_str()
-			    .ok_or(Error::new(
-				ErrorKind::Other, "couldn't get string from file name"))?);
-
-    Ok(file_path)
+    Ok(String::from(parent))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
 use rastrgraph::{self, Cli};
-#[allow(unused_imports)]
-use raster::{self, Color, Image};
 use structopt::StructOpt;
 
 #[allow(unused_variables)]
 fn main() {
     let args = Cli::from_args();
-    let path = rastrgraph::get_image_path(args.image).unwrap(); // Need to handle this error better than using unwrap, will use an `if let` statment with `ok_or_err`
-
-
-    let image = raster::open(&path);
-
-    println!("{}", path);
+    if let Err(e) = rastrgraph::run_args(args) {
+	eprintln!("an error has occured: {:?}",e);
+	std::process::exit(1);
+    }
     std::process::exit(0);
-
+    
 }
